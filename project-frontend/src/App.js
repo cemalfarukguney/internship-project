58b34f637f3b3d0273f181066fd137171502f3e9
Kemal Sezgen <kemalsezgen1@gmail.com>
2022-08-08 12:56:11 +0300

    get request

--- project-frontend/src/App.js
+++ project-frontend/src/App.js
@@ -2,14 +2,26 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import Navbar from './components/Navbar';
 import MainBody from './components/MainBody';
-import {useCallback, useState} from 'react';
+import {useCallback, useState, useEffect} from 'react';
+import axios from "axios";
 import { UserContext } from './UserContext';
 import TaskListContext from './context/TaskListContext';
 import defaultTasks from './tasks';
 import omitUndefined from './utils/omit-undefined';
+import CreateGameForm from './components/CreateGameForm';
 
 function App() {
+
+  const [issues, setIssues] = useState([]);
   const [tasks, setTasks] = useState(defaultTasks);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const response = await axios.get('http://localhost:8080/issues');
+      setIssues(response.data);
+    }
+    fetchData();
+  }, []);
 
   const[username, setUsername] = useState("Name");
   const[roomName, setRoomName] = useState("Room");
@@ -47,6 +59,17 @@ function App() {
         <TaskListContext.Provider value={[tasks, setTasks, updateTask]}>
           <Navbar parentCallback={callbackFunction}/>
           <MainBody taskState={state} />
+          <CreateGameForm />
+          <div>
+            <ul>
+              {issues.map(issue => (
+                <div key = {issue.id} style={{border: "1px red solid"}}> 
+                  <h2>{issue.name}</h2>
+                  <h4>{issue.description}</h4>
+                </div>
+              ))}
+            </ul>
+          </div>
         </TaskListContext.Provider>
       </UserContext.Provider>
     </div>
---
+++ project-frontend/src/components/CreateGameForm.js
@@ -0,0 +1,34 @@
+import React from "react";
+import { useState } from "react";
+import { Button, Modal, Form } from "react-bootstrap";
+
+export default function CreateGameForm() {
+  const [show, setShow] = useState(false);
+  const handleClose = () => setShow(false);
+  const handleShow = () => setShow(true);
+
+  return (
+    <div>
+      <Button
+        variant="info"
+        onClick={handleShow}
+      >
+        Create New Game
+      </Button>
+
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Body>
+          <Form>
+            <Form.Group className="mb-3" controlId="formBasicEmail">
+              <Form.Label aria-required><h3>Game Name</h3></Form.Label>
+              <Form.Control type="text" placeholder="Example Game Name" />
+            </Form.Group>
+          </Form>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary">Create</Button>
+        </Modal.Footer>
+      </Modal>
+    </div>
+  );
+}
